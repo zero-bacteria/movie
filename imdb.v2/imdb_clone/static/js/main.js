93e53b7c997204d980214d05e5dae371c10e4c49
bumhee <qqq960909@likelion.org>
2021-05-24 14:10:07 +0900

    front setting

---
+++ imdb.v2/imdb_clone/static/js/main.js
@@ -0,0 +1,179 @@
+ /*
+	Indus by TEMPLATE STOCK
+	templatestock.co @templatestock
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+/* ------------------------------------------------------------------------------
+ This is jquery module for main page
+ ------------------------------------------------------------------------------ */
+
+ /* Global constants */
+
+ /*global jQuery */
+ jQuery(function ($) {
+  'use strict';
+
+
+
+  var App = {
+    $options: {},
+    $loader: $(".loader"),
+    $animationload: $(".animationload"),
+    $countdown: $('#countdown_dashboard'),
+
+    bindEvents: function() {
+      //binding events
+      $(window).on('load', this.load.bind(this));
+      $(document).on('ready', this.docReady.bind(this));
+    },
+    load: function() {
+      /* ==============================================
+      1.Page Preloader
+      =============================================== */
+      this.$loader.delay(300).fadeOut();
+      this.$animationload.delay(600).fadeOut("slow");
+    },
+    docReady: function() {
+      /* -----------------------------------------------------------------------
+        Countdown
+        ----------------------------------------------------------------------- */
+        this.$countdown.countDown({
+          targetDate: {
+            'day':    this.$options.day,
+            'month':  this.$options.month,
+            'year':   this.$options.year,
+            'hour':   this.$options.hour,
+            'min':    this.$options.min,
+            'sec':    this.$options.sec
+          },
+          omitWeeks: true
+        });
+
+      /* ==============================================
+      NiceScroll
+      =============================================== */
+      $("html").niceScroll({
+        scrollspeed: 50,
+        mousescrollstep: 38,
+        cursorwidth: 7,
+        cursorborder: 0,
+        autohidemode: true,
+        zindex: 9999999,
+        horizrailenabled: false,
+        cursorborderradius: 0
+      });
+
+      /* ==============================================
+      Parallax
+      =============================================== */
+      $(window).stellar({
+        horizontalScrolling: false,
+        responsive: true,
+        scrollProperty: 'scroll',
+        parallaxElements: false,
+        horizontalOffset: 0,
+        verticalOffset: 0
+      });
+
+      //custom app
+      
+    },
+    init: function (_options) {
+      $.extend(this.$options, _options);
+      this.bindEvents();
+    }
+  }
+
+  //Initializing the app
+  //passing the countdown date
+  App.init({day: 18, month: 2, year: 2016, hour: 11, min: 59, sec: 59});
+});
+
+$(function() {
+
+  var siteSticky = function() {
+		$(".js-sticky-header").sticky({topSpacing:0});
+	};
+	siteSticky();
+
+	var siteMenuClone = function() {
+
+		$('.js-clone-nav').each(function() {
+			var $this = $(this);
+			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+		});
+
+
+		setTimeout(function() {
+			
+			var counter = 0;
+      $('.site-mobile-menu .has-children').each(function(){
+        var $this = $(this);
+        
+        $this.prepend('<span class="arrow-collapse collapsed">');
+
+        $this.find('.arrow-collapse').attr({
+          'data-toggle' : 'collapse',
+          'data-target' : '#collapseItem' + counter,
+        });
+
+        $this.find('> ul').attr({
+          'class' : 'collapse',
+          'id' : 'collapseItem' + counter,
+        });
+
+        counter++;
+
+      });
+
+    }, 1000);
+
+		$('body').on('click', '.arrow-collapse', function(e) {
+      var $this = $(this);
+      if ( $this.closest('li').find('.collapse').hasClass('show') ) {
+        $this.removeClass('active');
+      } else {
+        $this.addClass('active');
+      }
+      e.preventDefault();  
+      
+    });
+
+		$(window).resize(function() {
+			var $this = $(this),
+				w = $this.width();
+
+			if ( w > 768 ) {
+				if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+			}
+		})
+
+		$('body').on('click', '.js-menu-toggle', function(e) {
+			var $this = $(this);
+			e.preventDefault();
+
+			if ( $('body').hasClass('offcanvas-menu') ) {
+				$('body').removeClass('offcanvas-menu');
+				$this.removeClass('active');
+			} else {
+				$('body').addClass('offcanvas-menu');
+				$this.addClass('active');
+			}
+		}) 
+
+		// click outisde offcanvas
+		$(document).mouseup(function(e) {
+	    var container = $(".site-mobile-menu");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+	    }
+		});
+	}; 
+	siteMenuClone();
+
+});
